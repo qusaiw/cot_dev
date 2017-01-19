4e5873dc5bc695bd8cdd6be2adcbd078f3fd5574
qusaiw <qusaiobaida@gmail.com>
2017-01-19 17:14:41 +0200

    Initial commit

---
+++ Main.py
@@ -0,0 +1,43 @@
+import re
+import pprint
+
+DEV_CATCH = re.compile(r'([a-z]\d\d_)?([a-z0-9]+)(_.+)?(_\dp?\d*)')
+COT_CATCH = re.compile(r'([A-Z]+[0-9]+_)([A-Z0-9]+)(_[A-Z0-9]+)?(_\dP?\d*)')
+SPECIAL_COT_CATCH = re.compile(r'([A-Z]+[0-9]+_)([A-Z]+)()(\d)*')
+my_list = []
+final = {}
+
+
+def extract(group):
+    pre = group.group(1).strip('_') if my_search.group(1) else None
+    family = group.group(2).strip('_') if my_search.group(2) else None
+    infix = group.group(3).strip('_') if my_search.group(3) else None
+    drive = group.group(4).strip('_') if my_search.group(4) else None
+    return pre, family, infix, drive
+with open('list.txt') as f:
+    for line in f:
+        my_list.append([line[:-1].split(':')[0], line[:-1].split(':')[1]])
+for item in my_list:
+    dev_name = item[0]
+    cot_name = item[1]
+    if dev_name.lower() != dev_name:
+        print "%s has lowercase letter. skipping cell"
+        continue
+    if cot_name.upper() != cot_name:
+        print "%s has lowercase letters, skipping cell" % cot_name
+        continue
+    my_search = DEV_CATCH.search(dev_name)
+    if not my_search:
+        print "unmatched name"
+        continue
+    dev_specs = extract(my_search)
+
+    my_search = COT_CATCH.search(cot_name)
+    if not my_search:
+        my_search = SPECIAL_COT_CATCH.search(cot_name)
+    if not my_search:
+        print "unmatched name"
+        continue
+    cot_specs = extract(my_search)
+    final[str(item)] = [dev_specs, cot_specs]
+pprint.pprint(final)
